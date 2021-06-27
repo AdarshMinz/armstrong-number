44ab5592cfed56dbd6d7b3c8cdb2be8cac0dbb1a
AdarshMinz <86564997+AdarshMinz@users.noreply.github.com>
2021-06-28 04:13:04 +0530

    Add files via upload
    third(3rd) commit

---
+++ assignment_3(3).cpp
@@ -0,0 +1,59 @@
+1. Which stream class is to only write on files ?
+1. ofstream
+2. ifstream
+3. fstream
+4. iostream
+
+ANS: OFSTREAM
+
+2. Which stream class is to only read from files ?
+1. ofstream
+2. ifstream
+3. fstream
+4. iostream
+
+ANS: IFSTREAM
+
+3. Which stream class is used to both read and write on files ?
+1. ofstream
+2. ifstream
+3. fstream
+4. iostream
+
+ANS: FSTREAM
+
+4. Which among following is used to open a file in binary mode ?
+1. ios:app
+2. ios::out
+3. ios::in
+4. ios::binary
+
+ANS: IOS:APP
+
+5. ios::trunc is used for ?
+1. If the file is opened for output operations and it already existed, no action
+is taken.
+2. If the file is opened for output operations and it already existed, its
+previous content is deleted and replaced by the new one.
+3. If the file is opened for output operations and it already existed, then a
+new copy is created.
+4. None of above
+
+ANS: NONE OF THE ABOVE
+
+ 6. Which is correct syntax ?
+1. myfile:open ("example.bin", ios::out);
+2. myfile.open ("example.bin", ios::out);
+3. myfile::open ("example.bin", ios::out);
+4. myfile.open ("example.bin", ios:out);
+
+ANS: MYFILE.OPEN("EXAMPLE.BIN", IOS::OUT);
+
+7. Which among following is correct syntax of closing a file in c++ ?
+
+1. myfile$close();
+2. myfile@close();
+3. myfile:close();
+4. myfile.close();
+
+ANS: MYFILE.CLOSE();
